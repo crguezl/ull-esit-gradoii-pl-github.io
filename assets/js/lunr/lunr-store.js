30331b77418dbfc1ac54153e57ad2903b3ad3e02
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-01-29 14:44:54 +0000

    deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Lunes 2022/02/14",
+        "excerpt":"Introducción a la Asignatura Procesadores de Lenguajes Horarios, Calendario académico y Tutorías Vías de Comunicación Chat vs Hangouts Organización GitHub Rellenar Cuestionario para identificar la Relación entre el usuario GitHub y el usuario ULL Google Plus: Comunidad PL Práctica Opcional: GitHub Campus Expert Práctica: GitHub Project Board Práctica: Visual Studio...","categories": ["clases"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/clases/2022/02/14/leccion.html",
+        "teaser": null
+      },{
+        "title": "Introduccion a Procesadores de Lenguajes",
+        "excerpt":" Organización de PL y Primeros Pasos      Organización de PL y Primeros Pasos   What is PL About?      What is PL About?   ","categories": ["temas"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/temas/introduccion-a-pl",
+        "teaser": null
+      },{
+        "title": "GitHub Campus Expert",
+        "excerpt":"Aceptación de la Práctica github-campus-expert Esta práctica tiene por objetivo mejorar las competencias transversales y establecer nuestro workflow habitual. La realización de lo que se pide respecto a los cursos es opcional pero la aceptación y el informe son obligatorios. Al aceptar la asignación, si no se ha creado ya...","categories": ["practicas"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/practicas/github-campus-expert",
+        "teaser": null
+      },{
+        "title": "GitHub Project Board",
+        "excerpt":"Descripción de la Práctica github-project-board Para cada práctica/asignación se crea un tablero kanban (GitHub Project Automated Kanban) asociado al repo. Cree además un project board automated kanban de organización para el seguimiento de sus prácticas durante el curso. Este project board se crea a nivel de organización y no de...","categories": ["practicas"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/practicas/github-project-board",
+        "teaser": null
+      },{
+        "title": "GitPod and Visual Studio Code",
+        "excerpt":"Descripción de la Práctica vscode Objetivos Estudie y configure GitPod para trabajar con GitHub Instale VSCode en su máquina Siga los tutoriales de VSCode Instale el plugin Remote SSH y aprenda a usarlo Use Live Share para colaborar con un compañero Elabore un informe con la experiencia adquirida en el...","categories": ["practicas"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/practicas/visual-studio-code",
+        "teaser": null
+      },{
+        "title": "IAAS",
+        "excerpt":"Descripción de la Práctica iaas Siguiendo las instrucciones en el repositorio SYTW/iaas-ull-es despliegue un ejemplo como el que aparece en crguezl/express-start en su máquina virtual del servicio iaas.ull.es. Puede encontrar un vídeo del profesor introduciendo el iaas.ull.es aquí. El vídeo es de 2018 y varias de las apps usadas han...","categories": ["practicas"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/practicas/iaas",
+        "teaser": null
+      }]
---
+++ assets/temas/introduccion-a-javascript/zsh.md
@@ -0,0 +1,15 @@
+# zsh
+
+## Actualización de zsh
+
+Open up Terminal
+
+* Type in `cd ~/.oh-my-zsh`
+This should change your directory to the zsh directory.
+
+
+* Type in `git status` to see that your status is ahead of master.
+
+* Solve the conflicts
+
+* Type `upgrade_oh_my_zsh`
